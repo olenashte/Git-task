64825f45fdc967d07b768d78d4ac4875b829ebea
Olena <voloshinalena13@gmail.com>
2024-10-20 12:11:55 +0300

    Added comment elif

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def multiply(a, b):
 operation = input("Enter operation (+,* or -): ")
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-
+#Add elif
 if operation == "+":
     print("Result:", add(num1, num2))
 elif operation == "-":
