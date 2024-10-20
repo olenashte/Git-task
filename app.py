cae612dd7d88eed6bd46843bf4dafef4b5b35174
Olena <voloshinalena13@gmail.com>
2024-10-20 12:17:10 +0300

    Add new comment

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ if operation == "+":
     print("Result:", add(num1, num2))
 elif operation == "-":
     print("Result:", subtract(num1, num2))
+#elif operation == "*":
+    #print(multiply(num1, num2))
 elif operation == "*":
     print(multiply(num1, num2))
 else:
