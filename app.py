656d6431188d8bab3e2712dc6c619a457f597d41
Olena <voloshinalena13@gmail.com>
2024-10-20 10:39:34 +0300

    Delete operation mult

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
-    return a * b
-
-operation = input("Enter operation (+,* or -): ")
+operation = input("Enter operation (+ or -): ")
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
@@ -15,7 +12,6 @@ if operation == "+":
     print("Result:", add(num1, num2))
 elif operation == "-":
     print("Result:", subtract(num1, num2))
-elif operation == "*":
-    print(multiply(num1, num2))
+
 else:
     print("Unknown operation")
